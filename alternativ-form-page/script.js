470c0b4038301d8bf3dcfcc5d3fd7548f388bcc4
Devrim Akbulut <devrimakb1@gmail.com>
2023-11-26 17:52:13 +0100

    javascript added

---
+++ alternativ-form-page/script.js
@@ -0,0 +1,46 @@
+const todoForm = document.querySelector('#todoForm')
+const todoTitle = document.querySelector('#todoTitle')
+const todoText = document.querySelector('#todoText')
+
+
+/* Event listener */
+todoForm.addEventListener('submit', e => {
+    e.preventDefault()
+
+    validateText(todoTitle)
+    validateText(todoText)
+})
+
+/* Error meddelande funktion, tar inputens parent och lägger till klassen
+is-invalid, skriver även ut meddelandet */
+function setError(input, message) {
+    const parent = input.parentElement;
+    // classList.add() lägger bara till klassen om den inte redan finns
+    parent.classList.add('is-invalid')
+    parent.classList.remove('is-valid')
+    const errorElement = parent.querySelector('.invalid-input')
+    errorElement.innerText = message
+}
+
+/* Success funktion, här behövs inget meddelande */
+function setSuccess(input) {
+    const parent = input.parentElement;
+    // classList.add() lägger bara till klassen om den inte redan finns
+    parent.classList.remove('is-invalid')
+    parent.classList.add('is-valid')
+}
+
+/* validerar titeln*/
+function validateText(input) {
+    if(input.value.trim() === '') {
+        setError(input, 'Title cant be empty')
+        return
+    }
+    else if (input.value.trim().length <= 1) {
+        setError(input, 'Title cant contain less than 2 characters')
+        return
+    }
+
+    // Success 
+    setSuccess(input)
+}

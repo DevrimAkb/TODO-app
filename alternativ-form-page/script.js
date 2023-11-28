cda1c4a422782024285fba5a2c774ce989034c8c
Devrim Akbulut <devrimakb1@gmail.com>
2023-11-28 17:11:42 +0100

    renderposts done ish

--- alternativ-form-page/script.js
+++ alternativ-form-page/script.js
@@ -1,89 +1,105 @@
 const todoForm = document.querySelector('#todoForm')
-const todoTitle = document.querySelector('#todoTitle')
 const todoText = document.querySelector('#todoText')
-
 
 /* Event listener */
 todoForm.addEventListener('submit', e => {
     e.preventDefault()
-
-    validateTitle(todoTitle)
-    validateTodo(todoText) 
-
-   // En if sats som gör så att båda funktionerna måste gå igenom för att todon ska skickas
-     if(validateTitle(todoTitle) &&
-     validateTodo(todoText)) {
-         console.log('todo skickad')
-     }
-     else {
-         console.log('Du måste fylla i')
-     }
+    validateTodo(todoText)
+    createTodo() 
 })
 
+//Funktion för att skapa todos och skicka till API
 
+async function createTodo() {
+        const title = todoText.value
 
-/* Error meddelande funktion, tar inputens parent och lägger till klassen
-is-invalid, skriver även ut meddelandet */
+        const post = {
+            title
+        }
+         console.log(post)
+        const res = await fetch("https://js1-todo-api.vercel.app/api/todos?apikey=645478ef-292e-4731-ab3e-6aba10a07aa8", {
+            method: "POST",
+            headers: {
+                "Content-type": "application/json",
+            },
+            body: JSON.stringify(post)
+        })
+    
+         console.log(res)
+ }
+
+ /* validerar todo */
+ function validateTodo(input) {
+     if(input.value.trim() === '') {
+         setError(input, 'Todo cant be empty')
+         return false
+        }
+        else if (input.value.trim().length <= 1) {
+            setError(input, 'Todo cant contain less than 2 characters')
+            return false
+        }
+        
+        setSuccess(input)
+        return true
+    }
+    
+    /* Error meddelande funktion, tar inputs parent och lägger till klassen
+    is-invalid, skriver även ut meddelandet */
 function setError(input, message) {
     const parent = input.parentElement;
-    // classList.add() lägger bara till klassen om den inte redan finns
     parent.classList.add('is-invalid')
     parent.classList.remove('is-valid')
     const errorElement = parent.querySelector('.invalid-input')
     errorElement.innerText = message
 }
 
-/* Success funktion, här behövs inget meddelande */
 function setSuccess(input) {
     const parent = input.parentElement;
-    // classList.add() lägger bara till klassen om den inte redan finns
     parent.classList.remove('is-invalid')
     parent.classList.add('is-valid')
 }
-
-/* validerar titeln*/
-function validateTitle(input) {
-    if(input.value.trim() === '') {
-        setError(input, 'Title cant be empty')
-        return false
-    }
-    else if (input.value.trim().length <= 1) {
-        setError(input, 'Title cant contain less than 2 characters')
-        return false 
-    }
-
-    // Success 
-    setSuccess(input)
-    return true
-}
-
-/* validerar todo */
-function validateTodo(input) {
-    if(input.value.trim() === '') {
-        setError(input, 'Todo cant be empty')
-        return false
-    }
-    else if (input.value.trim().length <= 1) {
-        setError(input, 'Todo cant contain less than 2 characters')
-        return false
-    }
-
-    setSuccess(input)
-    return true
-}
-
 // -------------------------------------------------------------------------------------
 // Async api fetch
+let posts = []
 
-const getData = async () => {
-    try{
+const fetchPosts = async () => {
+    try {
         const res = await fetch('https://js1-todo-api.vercel.app/api/todos?apikey=645478ef-292e-4731-ab3e-6aba10a07aa8');
+        if(res.status !== 200) {
+            throw new Error('Something went wrong')
+        }
         const data = await res.json();
         console.log(data);
-        } catch (err) {
-        console.log(err)};
+        posts = data    
+        renderPosts();
+        } catch (error) {
+        console.log(error.message)};
     };
     
-    getData();
+fetchPosts();
+
+//------------------------------------------------------------------
+
+
+// Funktion för att skriva ut todos (EJ KLAR lektion 8 timestamp: 1.29.40)
+
+
+
+function renderPosts() {
+    const listContainer = document.querySelector('#list-container')
+    listContainer.innerHTML = ''
+
+    posts.forEach(post => {
+        listContainer.innerHTML += `
+        <ul id=list-container>
+        <li></li>
+        </ul>
+        ${post.title}
+        `
+    })
+}
+
+
+
 
 
